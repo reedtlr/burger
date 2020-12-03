7b44d3430b57083c7f18708a53c0f47202a77167
reedtlr <69362002+reedtlr@users.noreply.github.com>
2020-12-03 17:42:53 -0500

    created connection, DB, seeds, and server setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Big Veggie Bacon Supreme');
+INSERT INTO burgers (burger_name) VALUES ('Regular veggie burger');
+INSERT INTO burgers (burger_name) VALUES ('Kids veggie burger');
