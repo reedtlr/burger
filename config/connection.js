f5ebcce348401a788ce57851b9ab574aa0ee6339
reedtlr <69362002+reedtlr@users.noreply.github.com>
2020-12-07 18:15:21 -0500

    removed localhost, left jaws

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,4 @@
-// Set up MySQL connection.
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
-
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
@@ -25,9 +6,8 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     user: "root",
     password: "root",
-    database: "burgers_db"
+    database: "burgers_db",
+    port: 3306
   });
 };
-
-// Export connection for our ORM to use.
 module.exports = connection;
